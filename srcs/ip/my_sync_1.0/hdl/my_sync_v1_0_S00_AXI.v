7c6bb0d1534c176f933e8c3e60e2f321677c3a57
stdio2016 <randomfrank8@gmail.com>
2018-06-05 15:43:11 +0800

    make a mutex

--- srcs/ip/my_sync_1.0/hdl/my_sync_v1_0_S00_AXI.v
+++ srcs/ip/my_sync_1.0/hdl/my_sync_v1_0_S00_AXI.v
@@ -230,12 +230,14 @@
 	      begin
 	        case ( axi_awaddr[ADDR_LSB+OPT_MEM_ADDR_BITS:ADDR_LSB] )
 	          2'h0:
-	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
+	            /*for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
 	                // Respective byte enables are asserted as per write strobes 
 	                // Slave register 0
 	                slv_reg0[(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
-	              end  
+	              end  */
+	            slv_reg0 <= slv_reg0 == 0 ? S_AXI_WDATA :
+	              (slv_reg0 == S_AXI_WDATA ? 0 : slv_reg0);
 	          2'h1:
 	            for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
 	              if ( S_AXI_WSTRB[byte_index] == 1 ) begin
